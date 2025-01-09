6ccfff33f122ed97763c525ff7c1815fbbe7e550
Bovt Dmytro <mr.bovt2004@gmail.com>
2025-01-09 22:58:02 +0200

    done header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import './App.css';
+import './App.scss';
+import { Header } from './components/Header';
 
 export const App = () => {
   return (
     <>
+      <Header />
       <div className="container">
         <Outlet />
       </div>
