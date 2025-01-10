23da1af8e8c7a095953e00d2d2fd8b64c874ab2e
taniabarkovskya <taniabarkovskya@gmail.com>
2025-01-10 20:46:49 +0200

    implement redux toolkit and persist

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,25 +3,35 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { App } from './App';
 import { PhonesPage } from './components/PhonesPage/PhonesPage';
 import { NotFoundPage } from './components/NotFoundPage';
+import { Provider } from 'react-redux';
+import { persistor, store } from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export const Root = () => (
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<App />}
-        >
-          <Route
-            path="phones"
-            element={<PhonesPage />}
-          />
-          <Route
-            path="*"
-            element={<NotFoundPage />}
-          />
-        </Route>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <PersistGate
+        loading={null}
+        persistor={persistor}
+      >
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={<App />}
+            >
+              <Route
+                path="phones"
+                element={<PhonesPage />}
+              />
+              <Route
+                path="*"
+                element={<NotFoundPage />}
+              />
+            </Route>
+          </Routes>
+        </Router>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
