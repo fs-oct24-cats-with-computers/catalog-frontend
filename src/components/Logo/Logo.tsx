8409f8924308ec11c199f5e72f20634bd4c9409d
taniabarkovskya <taniabarkovskya@gmail.com>
2025-01-10 16:22:39 +0200

    implement router for all page + fix routes in components

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import logoIcon from '../../images/icons/Logo.svg';
 import './Logo.scss';
 
 export const Logo = () => (
-  <a href="/">
+  <Link to="/home">
     <img
       src={logoIcon}
       alt="Logo"
       className="Logo"
     />
-  </a>
+  </Link>
 );
