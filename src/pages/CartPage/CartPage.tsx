8a2f6bdf0b060d913bb70062cc93415a2b70aafb
taniabarkovskya <taniabarkovskya@gmail.com>
2025-01-11 22:49:55 +0200

    implement cart and favorites empty components

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,8 +1,11 @@
+import { CartEmpty } from '../../components/CartEmpty';
+
 export const CartPage = () => {
   return (
     <>
       <div className="page container">
         <h1 className="page__title">Cart</h1>
+        <CartEmpty />
       </div>
     </>
   );
--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,8 +1,11 @@
+import { FavoritesEmpty } from '../../components/FavoritesEmpty';
+
 export const FavoritesPage = () => {
   return (
     <>
       <div className="page container">
         <h1 className="page__title">Favorites</h1>
+        <FavoritesEmpty />
       </div>
     </>
   );
