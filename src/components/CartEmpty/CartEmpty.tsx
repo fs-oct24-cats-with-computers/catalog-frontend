8541e1ec2019e847ccaab61bd9642848e51554a2
taniabarkovskya <taniabarkovskya@gmail.com>
2025-01-13 11:55:08 +0200

    remove react fragment from empty pages

--- src/components/CartEmpty/CartEmpty.tsx
+++ src/components/CartEmpty/CartEmpty.tsx
@@ -2,18 +2,16 @@ import './CartEmpty.scss';
 
 export const CartEmpty = () => {
   return (
-    <>
-      <div className="empty">
-        <img
-          className="empty__image"
-          src="../../../img/cart-is-empty.png"
-          alt="Empty cart image"
-        />
-        <h2 className="empty__title">Cart is empty</h2>
-        <p className="empty__description">
-          Looks like you haven&apos;t made your choice yet....
-        </p>
-      </div>
-    </>
+    <div className="empty">
+      <img
+        className="empty__image"
+        src="../../../img/cart-is-empty.png"
+        alt="Empty cart image"
+      />
+      <h2 className="empty__title">Cart is empty</h2>
+      <p className="empty__description">
+        Looks like you haven&apos;t made your choice yet....
+      </p>
+    </div>
   );
 };
