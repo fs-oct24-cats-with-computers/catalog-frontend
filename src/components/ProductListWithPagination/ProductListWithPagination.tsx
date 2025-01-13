15bb2da7e38eedfac705c93e25eaac576b8ccfff
Maria <m.gosp1704@gmail.com>
2025-01-13 17:22:09 +0200

    implemented pagination

---
+++ src/components/ProductListWithPagination/ProductListWithPagination.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react';
+import { Product } from '../../types/Product';
+import ReactPaginate from 'react-paginate';
+// import { ProductCard } from '../ProductCard/ProductCard';
+
+type PropsType = {
+  products: Product[];
+};
+
+export const ProductsListWithPagination: React.FC<PropsType> = (props) => {
+  const { products } = props;
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const [currentItems, setCurrentItems] = useState<Product[]>([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(16);
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(products.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(products.length / itemsPerPage));
+  }, [itemsPerPage, itemOffset, products]);
+
+  interface PaginationAction {
+    selected: number;
+  }
+
+  const handlePageClick = (event: PaginationAction) => {
+    const newOffset = (event.selected * itemsPerPage) % products.length;
+    setItemOffset(newOffset);
+  };
+
+  return (
+    <>
+      {/* filters */}
+      <label
+        htmlFor="itemsPerPage"
+        className="page__selectionLabel"
+      >
+        Items per Page
+      </label>
+      <select
+        id="itemsPerPage"
+        value={itemsPerPage}
+        className="page__select"
+        onChange={(e) => setItemsPerPage(Number(e.target.value))}
+      >
+        <option value={16}>16</option>
+        <option value={32}>32</option>
+        <option value={64}>64</option>
+      </select>
+
+      {/* <div className="page__productList"> */}
+      {currentItems.map((phone) => (
+        <p
+          key={phone.id}
+          className="page__item"
+        >
+          {phone.name}
+        </p>
+        // <ProductCard key={phone.id} product={phone}/>
+      ))}
+      {/* </div> */}
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel=">"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="<"
+        renderOnZeroPageCount={null}
+        containerClassName="pagination"
+        pageLinkClassName="pagination-page" // page button
+        activeLinkClassName="pagination-active" // active page button
+        previousLinkClassName="pagination-prev" // 'back' page button
+        nextLinkClassName="pagination-next" // 'next' page button
+        disabledClassName="pagination-disabled" // disabled pahe button
+        breakClassName="pagination-break"
+      />
+    </>
+  );
+};
