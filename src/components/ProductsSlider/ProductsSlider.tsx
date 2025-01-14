276729def2349e74e3db61d1fec3341130a77cea
Nataliia Fedorchuk <natali.fedorchuk2211@gmail.com>
2025-01-14 22:06:40 +0200

    fix sliders

--- src/components/ProductsSlider/ProductsSlider.tsx
+++ src/components/ProductsSlider/ProductsSlider.tsx
@@ -2,30 +2,69 @@ import './ProductsSlider.scss';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
+// import { ProductCard } from '../ProductCard/ProductCard';
+// import { Product } from '../../types/Product';
+
+// type Props = {
+//   products: Product[];
+//   title: string;
+// };
+
+// export const ProductsSlider: React.FC<Props> = ({ products }) => {
 
 export const ProductsSlider: React.FC = () => {
   return (
     <section className="sliderProducts__wrapper">
       <div className="sliderProducts__title">
-        <h2>Brand new models</h2>
+        <h2 className="sliderProducts__title-content">Brand new models</h2>
         <div className="swiper-buttons-wrapper">
-          <button className="swiper-button-prev-new-models"></button>
-          <button className="swiper-button-next-new-models"></button>
+          <button className="swiper-button-prev"></button>
+          <button className="swiper-button-next"></button>
         </div>
       </div>
 
       <Swiper
         modules={[Navigation]}
         spaceBetween={16}
-        slidesPerView={4}
+        slidesPerView={1.5}
         navigation={{
-          nextEl: `.swiper-button-next-new-models`,
-          prevEl: `.swiper-button-prev-new-models`,
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2.5,
+            spaceBetween: 16,
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 16,
+          },
         }}
       >
-        <SwiperSlide></SwiperSlide>
+        <SwiperSlide className="ProductWrapper">
+          <div>Slide 2</div>
+        </SwiperSlide>
+        <SwiperSlide className="ProductWrapper">
+          <div>Slide 2</div>
+        </SwiperSlide>
+        <SwiperSlide className="ProductWrapper">
+          <div>Slide 2</div>
+        </SwiperSlide>
+        <SwiperSlide className="ProductWrapper">
+          <div>Slide 2</div>
+        </SwiperSlide>
+        <SwiperSlide className="ProductWrapper">
+          <div>Slide 2</div>
+        </SwiperSlide>
+        {/* {products.map(product => (
+          <SwiperSlide key={product.id} className="ProductWrapper">
+            <ProductCard product={product} />
+          </SwiperSlide>
+        ))} */}
       </Swiper>
     </section>
   );
