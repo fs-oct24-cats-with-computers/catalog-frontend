81a1bcc0418d3938ec9974f5c1e11c724195996f
Maria <m.gosp1704@gmail.com>
2025-01-15 21:56:28 +0200

    implemented tablets and accesories page

--- src/pages/AccessoriesPage/AccessoriesPage.tsx
+++ src/pages/AccessoriesPage/AccessoriesPage.tsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from 'react';
+import { Product } from '../../types/Product';
+import { getProducts } from '../../api';
+import { Breadcrumbs } from '../../components/Breadcrumbs';
+import { ProductsListWithPagination } from '../../components/ProductListWithPagination';
+
 export const AccessoriesPage = () => {
+  const [accessories, setAccessories] = useState<Product[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getProducts()
+      .then((products) =>
+        setAccessories(
+          products.filter((product) => product.category === 'accessories'),
+        ),
+      )
+      .catch(() => setError('Something went wrong'));
+  }, []);
+
+  if (error) {
+    <p>{error}</p>;
+  }
+
   return (
     <>
       <div className="page container">
+        <Breadcrumbs />
         <h1 className="page__title">Accessories</h1>
+        <p className="page__subtitle">{accessories.length} models</p>
+        <ProductsListWithPagination products={accessories} />
       </div>
     </>
   );
