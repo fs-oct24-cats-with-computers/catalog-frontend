980fe14b1d04171d9735979b4ea27889d78abaab
Maria <m.gosp1704@gmail.com>
2025-01-20 23:08:11 +0200

    implemented dark mode for empty pages, burger menu, productDetails, Back

--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -45,10 +45,6 @@ export const ProductsPage: React.FC<Props> = ({ type }) => {
       });
   }, [type]);
 
-  const handleThemeClick = () => {
-    setTheme(theme === 'light-mode' ? 'dark-mode' : 'light-mode');
-  };
-
   if (error) {
     return <NotFoundPage />;
   }
