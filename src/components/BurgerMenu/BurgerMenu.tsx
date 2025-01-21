9f703987f1ba98e700b5acd783018b95626311f7
tania <68154918+taniabarkovskya@users.noreply.github.com>
2025-01-21 10:31:25 +0200

    Feature/implement rights page (#41)
    * implement rights page
    
    * change site logo and title
    
    * fix footer link to github
    
    * fix empty pages layout
    
    * add counter to menu burger
    
    * fix header and burger menu counter position

--- src/components/BurgerMenu/BurgerMenu.tsx
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import './BurgerMenu.scss';
 import React from 'react';
+import { useAppSelector } from '../../app/hooks';
 
 interface Props {
   isBurgerMenuOpen: boolean;
@@ -11,6 +12,9 @@ export const BurgerMenu: React.FC<Props> = ({
   isBurgerMenuOpen,
   setIsBurgerMenu,
 }) => {
+  const favoriteProducts = useAppSelector((state) => state.favoriteProducts);
+  const cartProducts = useAppSelector((state) => state.cartProducts);
+
   const chooseActivePage = ({ isActive }: { isActive: boolean }) =>
     isActive ? 'burger__nav_link burger__is-active' : 'burger__nav_link';
 
@@ -71,6 +75,11 @@ export const BurgerMenu: React.FC<Props> = ({
             src="img/header_components/Favourites.svg"
             alt="Favourites"
           />
+          {!!favoriteProducts.length && (
+            <span className="header__buttons_element--state">
+              {favoriteProducts.length}
+            </span>
+          )}
         </NavLink>
         <NavLink
           to="/cart"
@@ -81,6 +90,11 @@ export const BurgerMenu: React.FC<Props> = ({
             src="img/header_components/Shopping bag.svg"
             alt="Shopping bag"
           />
+          {!!cartProducts.length && (
+            <span className="header__buttons_element--state">
+              {cartProducts.length}
+            </span>
+          )}
         </NavLink>
       </div>
     </section>
