b979e4bc07d067f102cbfcfe6350e9ab76268c40
mgospodinova <147430782+Mariagosp@users.noreply.github.com>
2025-01-21 08:59:17 +0200

    implemented skeleton on products page (#37)
    * implemented skeleton on products page
    
    * implemented skeleton on products page
    
    * pulled changes

--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -10,6 +10,8 @@ import { useSortParams } from '../../hooks/useSortParams';
 import { itemsPerPageOptions, sortByOptions } from '../../utils/sortingArrays';
 import { Category } from '../../types/Category';
 import { pageTitle } from '../../utils/titleHelper';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 type Props = {
   type: Category;
@@ -18,6 +20,7 @@ type Props = {
 export const ProductsPage: React.FC<Props> = ({ type }) => {
   const [products, setProducts] = useState<Product[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   const title = pageTitle(type);
 
@@ -30,11 +33,15 @@ export const ProductsPage: React.FC<Props> = ({ type }) => {
   } = useSortParams(products);
 
   useEffect(() => {
+    setIsLoading(true);
     getProducts()
       .then((allProducts) =>
         setProducts(allProducts.filter((product) => product.category === type)),
       )
-      .catch(() => setError('Something went wrong'));
+      .catch(() => setError('Something went wrong'))
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, [type]);
 
   if (error) {
@@ -44,25 +51,59 @@ export const ProductsPage: React.FC<Props> = ({ type }) => {
   return (
     <>
       <div className="page container">
-        <Breadcrumbs />
-        <h1 className="page__title">{title}</h1>
-        <p className="page__subtitle">{products.length} models</p>
+        {isLoading ?
+          <Skeleton
+            className="breadcrumb-skeleton"
+            width={130}
+            height={20}
+          />
+        : <Breadcrumbs />}
+        <h1 className="page__title">
+          {isLoading ?
+            <Skeleton />
+          : title}
+        </h1>
+        <p className="page__subtitle">
+          {isLoading ?
+            <Skeleton />
+          : `${products.length} models`}
+        </p>
         <div className="page__dropdown">
           <div className="page__dropdown--sortBy">
-            <p className="page__dropdown--sortBy--label">Sort by</p>
-            <Dropdown
-              sort={sortBy}
-              handleSortBy={handleSortBy}
-              options={sortByOptions}
-            />
+            <p className="page__dropdown--sortBy--label">
+              {isLoading ?
+                <Skeleton width={65} />
+              : 'Sort by'}
+            </p>
+            {isLoading ?
+              <Skeleton
+                width={180}
+                height={40}
+              />
+            : <Dropdown
+                sort={sortBy}
+                handleSortBy={handleSortBy}
+                options={sortByOptions}
+              />
+            }
           </div>
           <div className="page__dropdown--items">
-            <p className="page__dropdown--items--label">Items on page</p>
-            <Dropdown
-              sort={itemsPerPage}
-              options={itemsPerPageOptions}
-              handleItemsPerPage={handleItemsPerPage}
-            />
+            <p className="page__dropdown--items--label">
+              {isLoading ?
+                <Skeleton width={65} />
+              : 'Items on page'}
+            </p>
+            {isLoading ?
+              <Skeleton
+                width={180}
+                height={40}
+              />
+            : <Dropdown
+                sort={itemsPerPage}
+                options={itemsPerPageOptions}
+                handleItemsPerPage={handleItemsPerPage}
+              />
+            }
           </div>
         </div>
         <ProductsListWithPagination
